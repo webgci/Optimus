15a170648b55f64395045c28e4fa2cdaacd6e295
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-01-15 19:41:50 -0200

    adding gitignore and the buttons partial

--- scss/config.rb
+++ scss/config.rb
@@ -4,12 +4,12 @@
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "/img"
+javascripts_dir = "/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-
+output_style = :compressed
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
